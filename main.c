ec15810e7b1715f9e3338596cacc3e7d902b1eb5
Victor.Manzini <victor@robertomanzini.com.br>
2026-03-17 11:54:34 -0300

    Aluno: Victor. Aqruivo compilado e .exe adicionado + correção na linha 5

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    printf("Aluno: Victor Manzini"); // Adicionado print f com o nome do usuario linha 4
+
+    printf("\nAluno: Victor Manzini\n"); // Adicionado print f com o nome do usuario linha 4
     printf("\nAluno: Luiz Salvador\n"); // Adicionado print f com o nome do aluno
-    printf("\nAluno:Guilherme Santos\n"); // Adicionado print f com o nome do usuario linha 6 
+    printf("\nAluno: Guilherme Santos\n"); // Adicionado print f com o nome do usuario linha 6 
     printf("\nAluno: Caio Marinho\n"); // Caio: Adicionado printf com nome do aluno
     printf("\nAluno: Ricardo Tunes\n"); // Comentario adicionado do Aluno Ricardo Tunes
-
-
 
     return 0;
 }
