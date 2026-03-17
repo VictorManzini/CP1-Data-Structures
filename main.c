2db4dba241ffc61fc4f58a06a6f238fb55089656
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 11:51:49 -0300

    Comentario adicionado na linha 4 Aluno: Guilherme Santos

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    printf("Aluno: Victor Manzini");
+    printf("Aluno: Victor Manzini"); // Adicionado print f com o nome do usuario linha 4
     printf("\nAluno: Luiz Salvador\n"); // Adicionado print f com o nome do aluno
-    printf("\nAluno:Guilherme Santos\n"); // Adicionado print f com o nome do usuario linha 6
+    printf("\nAluno:Guilherme Santos\n"); // Adicionado print f com o nome do usuario linha 6 
     printf("\nAluno: Caio Marinho\n"); // Caio: Adicionado printf com nome do aluno
     printf("\nAluno: Ricardo Tunes\n"); // Comentario adicionado do Aluno Ricardo Tunes
 
